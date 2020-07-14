633656e553d49df3f5bee99ce4d7486b58e38cb6
ahxie <ahxie@iu.edu>
2020-07-14 14:28:12 -0400

    finishing the web app

--- mask-simulator.js
+++ mask-simulator.js
@@ -1,3 +1,6 @@
+
+
+
 var data = JSON.parse(`[
     {
       "Material": "Al",
@@ -44,8 +47,12 @@ for (i = 0; i < data.length; i++) {
 // make a function to find optimal thickness after button is clicked 
 
 function determineThickness() {
+
+    var wavelength = +document.getElementById('wavelength').value;
+    var phaseShift = +document.getElementById('phaseShift').value;
+
     // find index of material in drop down 
-    var dropdown = document.getElementById('select').value;
+    var dropdown = document.getElementById('material-dropdown').value;
     var index = materialArray.indexOf(dropdown);
 
     // find corresponding delta 
@@ -55,35 +62,112 @@ function determineThickness() {
     var n2 = 1 + delta;
 
     // define phase shift variable
-    var phaseShift = document.getElementById('phaseShift').value;
 
     // convert phase shift value to radians if needed (not working)
-    if (document.getElementById('units').value === 'Radians') {
-        phaseShift = document.getElementById('phaseShift').value;
-    }  else if (document.getElementById('units').value === 'Degrees') {
-        phaseShift = document.getElementById('phaseShift').value*Math.PI/180;
-    } else if (document.getElementById('units') === 'Waves') {
-        phaseShift = document.getElementById('phaseShift').value*2*Math.PI;
+
+    switch(document.getElementById('units').value){
+      case 'radians':
+        //phaseShift = document.getElementById('phaseShift').value;
+      break
+      case 'degrees':
+        phaseShift *= Math.PI/180;
+      break
+      case 'waves':
+        phaseShift = phaseShift*2*Math.PI;
+      break
+      default:
+        console.log('(should never get here) No recognized units!')
     }
+
+ 
 
     // compute optimal thickness 
     const n1 = 1;
     var theta1 = 6*Math.PI/180;
     var theta2 = Math.asin(n1/n2*(Math.sin(theta1)));
-    var d = phaseShift*document.getElementById('wavelength').value/(4*Math.PI*(n1/Math.cos(theta1)-n2/Math.cos(theta2)));
+    var d = phaseShift*wavelength/(4*Math.PI*(n1/Math.cos(theta1)-n2/Math.cos(theta2)));
 
     let counter = 0;
     while (d < 0) {
         phaseShift = phaseShift-(2*Math.PI);
-        phaseShift*document.getElementById('wavelength').value/(4*Math.PI*(n1/Math.cos(theta1)-n2/Math.cos(theta2)));
+        d = phaseShift*wavelength/(4*Math.PI*(n1/Math.cos(theta1)-n2/Math.cos(theta2)));
         counter = counter+1;
-        if (counter>50) 
+
+        // we should never get in this loop, it's only to prevent chrome from crashing
+        if (counter>50){
+          console.log('counter has reached 50!')
             break
+        }
     }
 
     // display optimal thickness in text edit box (shows as negative?)
     document.getElementById('thickness').value = d;
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+
+  var select = document.getElementById('material-dropdown');
+    
+  // for(var i = 0; i < materialArray.length; i++) {
+  //     var option = document.createElement('option')
+  //     option.text = materialArray[i];
+  //     select.add(option);
+  // }
+
+
+  // working with elements of array 
+  materialArray.map( (material) =>{
+    var option = document.createElement('option')
+    option.text = material;
+    select.add(option);
+  })
+
+ 
+
+  // map, filter, reduce
+
+
+
+
+  // var squarer = function(x){
+  //   return x*x;
+  // }
+
+
+  var myArray = [0, 1, 2, 3];
+  var squaredArray = [];
+
+  // // option 1 (for loop, "imperative")
+  // for (var k = 0; k < myArray.length; k++){
+  //   squaredArray[k] =squarer(myArray[k]);
+  // }
+
+  
+
+  // // option 2 (no loop, "declarative")
+  // squaredArray = myArray.map(squarer);
+
+  // squaredArray = myArray.map(function(x){return x*x})
+
+
+
+  // (x) => {
+  //   console.log(x);
+  //   return x*x
+  // }
+
+
+  // squaredArray = myArray.map((x) => x*x)
+  // alert(squaredArray);
+
+  // myArray.map((x) => {console.log(x)})
+    
+});
+
+
 
 
 
